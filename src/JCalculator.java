0b7e6eaf8b5e0de70f868977bc7a3c21b536bfd5
scout407 <scout407serreto@gmail.com>
2018-11-28 09:57:16 +0100

    kisdvb

--- src/JCalculator.java
+++ src/JCalculator.java
@@ -1,143 +1,153 @@
+
+import operator.arithmetic.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
 import operator.*;
+import java.util.Stack;
 
-public class JCalculator extends JFrame
-{
-   // Tableau representant une pile vide
-   private final String[] empty = { "< empty stack >" };
+public class JCalculator extends JFrame {
+    // Tableau representant une pile vide
 
-   // Zone de texte contenant la valeur introduite ou resultat courant
-   private final JTextField jNumber = new JTextField("0");
+    private final String[] empty = {"< empty stack >"};
 
-   // Composant liste representant le contenu de la pile
-   private final JList jStack = new JList(empty);
+    // Zone de texte contenant la valeur introduite ou resultat courant
+    private final JTextField jNumber = new JTextField("0");
 
-   // Contraintes pour le placement des composants graphiques
-   private final GridBagConstraints constraints = new GridBagConstraints();
+    // Composant liste representant le contenu de la pile
+    private final JList jStack = new JList(empty);
 
+    // Contraintes pour le placement des composants graphiques
+    private final GridBagConstraints constraints = new GridBagConstraints();
 
-   /*
-    * Mise a jour de l'interface apres une operation (jList et jStack)
-    */
-   private void update()
-   {
-      // Modifier une zone de texte, JTextField.setText(string nom)
-      // Modifier un composant liste, JList.setListData(Object[] tableau)
-   }
+    private Stack<Double> stack = new Stack<Double>();
+    
 
-   /*
-    * Ajout d'un bouton dans l'interface et de l'operation associee,
-    * instance de la classe Operation, possedeant une methode execute()
-    */
-   private void addOperatorButton(String name, int x, int y, Color color,
-                                  final Operator operator)
-   {
-      JButton b = new JButton(name);
-      b.setForeground(color);
-      constraints.gridx = x;
-      constraints.gridy = y;
-      getContentPane().add(b, constraints);
+    /*
+     * Mise a jour de l'interface apres une operation (jList et jStack)
+     */
+    private void update() {
+        // Modifier une zone de texte, JTextField.setText(string nom)
+        // Modifier un composant liste, JList.setListData(Object[] tableau)
 
-      b.addActionListener(new ActionListener() {
-         public void actionPerformed(ActionEvent e) {
-            operator.execute();
-            update();
-         }});
-   }
+    }
 
-   /*
-    * Constructeur
-    */
-   public JCalculator()
-   {
-      super("JCalculator");
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      getContentPane().setLayout(new GridBagLayout());
+    /*
+   * Ajout d'un bouton dans l'interface et de l'operation associee,
+   * instance de la classe Operation, possedeant une methode execute()
+     */
+    private void addOperatorButton(String name, int x, int y, Color color,
+            final Operator operator) {
+        JButton b = new JButton(name);
+        b.setForeground(color);
+        constraints.gridx = x;
+        constraints.gridy = y;
+        getContentPane().add(b, constraints);
 
-      // Contraintes des composants graphiques
-      constraints.insets = new Insets(3, 3, 3, 3);
-      constraints.fill = GridBagConstraints.HORIZONTAL;
+        b.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                operator.execute();
+                update();
+            }
+        });
+    }
 
-      // Nombre courant
-      jNumber.setEditable(false);
-      jNumber.setBackground(Color.WHITE);
-      jNumber.setHorizontalAlignment(JTextField.RIGHT);
-      constraints.gridx = 0;
-      constraints.gridy = 0;
-      constraints.gridwidth = 5;
-      getContentPane().add(jNumber, constraints);
-      constraints.gridwidth = 1; // reset width
+    /*
+     * Constructeur
+     */
+    public JCalculator() {
+        super("JCalculator");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new GridBagLayout());
 
-      // Rappel de la valeur en memoire
-      addOperatorButton("MR", 0, 1, Color.RED, null);
+        // Contraintes des composants graphiques
+        constraints.insets = new Insets(3, 3, 3, 3);
+        constraints.fill = GridBagConstraints.HORIZONTAL;
 
-      // Stockage d'une valeur en memoire
-      addOperatorButton("MS", 1, 1, Color.RED, null);
+        // Nombre courant
+        jNumber.setEditable(false);
+        jNumber.setBackground(Color.WHITE);
+        jNumber.setHorizontalAlignment(JTextField.RIGHT);
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 5;
+        getContentPane().add(jNumber, constraints);
+        constraints.gridwidth = 1; // reset width
 
-      // Backspace
-      addOperatorButton("<=", 2, 1, Color.RED, null);
+        //---------------- memory -------------------------------------------------
+        // Rappel de la valeur en memoire
+        addOperatorButton("MR", 0, 1, Color.RED, null);
 
-      // Mise a zero de la valeur courante + suppression des erreurs
-      addOperatorButton("CE", 3, 1, Color.RED, null);
+        // Stockage d'une valeur en memoire
+        addOperatorButton("MS", 1, 1, Color.RED, null);
 
-      // Comme CE + vide la pile
-      addOperatorButton("C",  4, 1, Color.RED, null);
+        // Backspace
+        addOperatorButton("<=", 2, 1, Color.RED, null);
 
-      // Boutons 1-9
-      for (int i = 1; i < 10; i++)
-         addOperatorButton(String.valueOf(i), (i - 1) % 3, 4 - (i - 1) / 3,
-               Color.BLUE, null);
-      // Bouton 0
-      addOperatorButton("0", 0, 5, Color.BLUE, null);
+        // Mise a zero de la valeur courante + suppression des erreurs
+        addOperatorButton("CE", 3, 1, Color.RED, null);
 
-      // Changement de signe de la valeur courante
-      addOperatorButton("+/-", 1, 5, Color.BLUE, null);
+        // Comme CE + vide la pile
+        addOperatorButton("C", 4, 1, Color.RED, null);
 
-      // Operateur point (chiffres apres la virgule ensuite)
-      addOperatorButton(".", 2, 5, Color.BLUE, null);
+        // Entree: met la valeur courante sur le sommet de la pile
+        addOperatorButton("Ent", 4, 5, Color.RED, null);
 
-      // Operateurs arithmetiques a deux operandes: /, *, -, +
-      addOperatorButton("/", 3, 2, Color.RED, null);
-      addOperatorButton("*", 3, 3, Color.RED, null);
-      addOperatorButton("-", 3, 4, Color.RED, null);
-      addOperatorButton("+", 3, 5, Color.RED, null);
+        //---------------- Fin memory ---------------------------------------------
+        //---------------- number ------------------------------------------------
+        // Boutons 1-9
+        for (int i = 1; i < 10; i++) {
+            addOperatorButton(String.valueOf(i), (i - 1) % 3, 4 - (i - 1) / 3,
+                    Color.BLUE, null);
+        }
+        // Bouton 0
+        addOperatorButton("0", 0, 5, Color.BLUE, null);
 
-      // Operateurs arithmetiques a un operande: 1/x, x^2, Sqrt
-      addOperatorButton("1/x", 4, 2, Color.RED, null);
-      addOperatorButton("x^2", 4, 3, Color.RED, null);
-      addOperatorButton("Sqrt", 4, 4, Color.RED, null);
+        // Changement de signe de la valeur courante
+        addOperatorButton("+/-", 1, 5, Color.BLUE, null);
 
-      // Entree: met la valeur courante sur le sommet de la pile
-      addOperatorButton("Ent", 4, 5, Color.RED, null);
+        // Operateur point (chiffres apres la virgule ensuite)
+        addOperatorButton(".", 2, 5, Color.BLUE, null);
 
-      // Affichage de la pile
-      JLabel jLabel = new JLabel("Stack");
-      jLabel.setFont(new Font("Dialog", 0, 12));
-      jLabel.setHorizontalAlignment(JLabel.CENTER);
-      constraints.gridx = 5;
-      constraints.gridy = 0;
-      getContentPane().add(jLabel, constraints);
+        //----------------- Fin number -------------------------------------------
+        //----------------- aritmetic --------------------------------------------
+        // Operateurs arithmetiques a deux operandes: /, *, -, +
+        addOperatorButton("/", 3, 2, Color.RED, new Division());
+        addOperatorButton("*", 3, 3, Color.RED, new Multiplication());
+        addOperatorButton("-", 3, 4, Color.RED, new Substraction());
+        addOperatorButton("+", 3, 5, Color.RED, new Addition());
 
-      jStack.setFont(new Font("Dialog", 0, 12));
-      jStack.setVisibleRowCount(8);
-      JScrollPane scrollPane = new JScrollPane(jStack);
-      constraints.gridx = 5;
-      constraints.gridy = 1;
-      constraints.gridheight = 5;
-      getContentPane().add(scrollPane, constraints);
-      constraints.gridheight = 1; // reset height
+        // Operateurs arithmetiques a un operande: 1/x, x^2, Sqrt
+        addOperatorButton("1/x", 4, 2, Color.RED, new Reversion());
+        addOperatorButton("x^2", 4, 3, Color.RED, new Square());
+        addOperatorButton("Sqrt", 4, 4, Color.RED, new SquareRoot());
 
-      setResizable(false);
-      pack();
-   }
+        //----------------- Fin aritmetic -----------------------------------------
+        // Affichage de la pile
+        JLabel jLabel = new JLabel("Stack");
+        jLabel.setFont(new Font("Dialog", 0, 12));
+        jLabel.setHorizontalAlignment(JLabel.CENTER);
+        constraints.gridx = 5;
+        constraints.gridy = 0;
+        getContentPane().add(jLabel, constraints);
 
-   /*
-    * main()
-    */
-   public static void main(String args[]) {
-      new JCalculator().setVisible(true);
-   }
+        jStack.setFont(new Font("Dialog", 0, 12));
+        jStack.setVisibleRowCount(8);
+        JScrollPane scrollPane = new JScrollPane(jStack);
+        constraints.gridx = 5;
+        constraints.gridy = 1;
+        constraints.gridheight = 5;
+        getContentPane().add(scrollPane, constraints);
+        constraints.gridheight = 1; // reset height
+
+        setResizable(false);
+        pack();
+    }
+
+    /*
+   * main()
+     */
+    public static void main(String args[]) {
+        new JCalculator().setVisible(true);
+    }
 }
